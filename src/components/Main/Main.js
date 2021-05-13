73127b893702e74d349cfc627ebe6d9833f67159
Sumit H <ssumitofficial@gmail.com>
2021-05-13 17:06:13 +0530

    Code check in for footer

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,11 +1,13 @@
 import Header from '../Header/Header.jsx';
 import Home from '../Home/Home.jsx';
+import Footer from '../Footer/Footer.jsx';
 
 export default function Main() {
     return (
-        <div className="row">
+        <div>
             <Header></Header>
             <Home></Home>
+            <Footer></Footer>
         </div>
     );
 }
